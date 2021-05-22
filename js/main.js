11e04c56a60db45680598c4ba4c8f481d17d117c
Chuyiwa <soshappy123@gmail.com>
2021-05-23 00:58:04 +0800

    qwe

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 // Put variables in global scope to make them available to the browser console.
 const constraints = window.constraints = {
   audio: false,
-  video: { facingMode: { exact: "environment" } }
+  video: true
 };
 
 function handleSuccess(stream) {
